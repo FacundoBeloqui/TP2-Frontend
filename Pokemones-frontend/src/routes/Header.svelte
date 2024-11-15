f79bb056adc67c735f55e2ec6bb97113b177486d
Jean-Luka-Arena <jarena@fi.uba.ar>
2024-11-15 17:20:38 -0300

    creo las paginas y las linkeo

--- Pokemones-frontend/src/routes/Header.svelte
+++ Pokemones-frontend/src/routes/Header.svelte
@@ -72,8 +72,8 @@
 			</a>
 		</p>
 		<p class="main-menu-item">
-			<!-- svelte-ignore a11y_missing_attribute -->
-			<a class="main-menu-heading">
+		<!-- svelte-ignore a11y_missing_attribute -->
+			<a href="/Pokemones" class="main-menu-heading">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 512 512"
@@ -90,8 +90,8 @@
 			</a>
 		</p>
 		<p class="main-menu-item">
-			<!-- svelte-ignore a11y_missing_attribute -->
-			<a class="main-menu-heading">
+		<!-- svelte-ignore a11y_missing_attribute -->
+			<a href="/Movimientos" class="main-menu-heading">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 512 512"
@@ -110,7 +110,7 @@
 		</p>
 		<p class="main-menu-item">
 			<!-- svelte-ignore a11y_missing_attribute -->
-			<a class="main-menu-heading">
+			 <a href="/Equipos" class="main-menu-heading">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 512 512"
