78772e2c365c2b51ce3b5f74bf932c673a2410fd
FacundoBeloqui <facu.beloqui04@gmail.com>
2024-11-18 15:26:38 -0300

    Agregada la llamada al get-pokemon

---
+++ Pokemones-frontend/src/routes/pokemones/+page.server.js
@@ -0,0 +1,34 @@
+export async function load() {
+	let url = new URL('http://localhost:8000/pokemones/');
+	const response = await fetch(url);
+	if (!response.ok) {
+		throw new Error(`Response status: ${response.status}`);
+	}
+
+	let pokemones = await response.json();
+
+	return {
+		pokemones: pokemones
+	};
+}
+/*
+export const actions = {
+	create: async ({ request }) => {
+		const data = await request.formData();
+
+		let url = new URL('http://localhost:8000/alumnos/');
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				nombre: data.get('nombre'),
+				apellido: data.get('apellido'),
+				edad: data.get('edad')
+			})
+		});
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+	}
+};
+*/
--- Pokemones-frontend/src/routes/pokemones/+page.svelte
+++ Pokemones-frontend/src/routes/pokemones/+page.svelte
@@ -1,1 +1,73 @@
-<h1>esta es la pagina de pokemones</h1>
+<script>
+	export let data;
+</script>
+
+<h1>Ésta es la página de pokemones</h1>
+
+<table>
+	<thead>
+		<tr>
+			<th>Id</th>
+			<th>Identificador</th>
+			<th>Altura</th>
+			<th>Peso</th>
+			<th>Experiencia Base</th>
+			<th>Imagen</th>
+			<th>Grupo de Huevo</th>
+			<th>Generacion</th>
+			<th>Habilidades</th>
+			<th>Evoluciones Inmediatas</th>
+			<th>Tipo</th>
+			<th>Estadisticas</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each data.pokemones as pokemon}
+			<tr class={pokemon.id % 2 == 0 ? 'zero' : 'one'}>
+				<td>{pokemon.id}</td>
+				<td>{pokemon.identificador}</td>
+				<td>{pokemon.altura}</td>
+				<td>{pokemon.peso}</td>
+				<td>{pokemon.experiencia_base}</td>
+				<td><img src={pokemon.imagen} alt="imagen de {pokemon.identificador}" /></td>
+				<td>{pokemon.grupo_de_huevo}</td>
+				<td>{pokemon.generacion}</td>
+				<td>{pokemon.habilidades}</td>
+				<td>{pokemon.evoluciones_inmediatas}</td>
+				<td>{pokemon.tipo}</td>
+				<td>{Object.keys(pokemon.estadisticas).map((key) => [key, pokemon.estadisticas[key]])}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<!--
+<h2>Dar de alta alumno</h2>
+<form method="POST" action="?/create">
+	<label>
+		Nombre:
+		<input name="nombre" autocomplete="off" />
+	</label>
+	<label>
+		Apellido:
+		<input name="apellido" autocomplete="off" />
+	</label>
+	<label>
+		Edad:
+		<input name="edad" autocomplete="off" />
+	</label>
+
+	<button>Crear</button>
+</form>
+-->
+<style>
+	tr {
+		text-align: center;
+	}
+	.zero {
+		background-color: rgb(172, 169, 169);
+	}
+	.one {
+		background-color: white;
+	}
+</style>
