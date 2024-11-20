19b036fce93bb7b4199cb3e679301e5e97f472c9
FacundoBeloqui <facu.beloqui04@gmail.com>
2024-11-20 14:24:32 -0300

    Agregado estilo al pokemon/id

--- Pokemones-frontend/src/routes/pokemones/[id]/+page.server.js
+++ Pokemones-frontend/src/routes/pokemones/[id]/+page.server.js
@@ -13,36 +13,3 @@ export async function load({ params }) {
 		pokemon
 	};
 }
-/*
-export const actions = {
-	desinscribir: async ({ request }) => {
-		const data = await request.formData();
-
-		let url = new URL(`http://localhost:8000/pokemons/${data.get('id')}/desinscribir_alumno/`);
-		let params = { padron: data.get('padron') };
-		url.search = new URLSearchParams(params).toString();
-
-		const response = await fetch(url, {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' }
-		});
-		if (!response.ok) {
-			error(response.status, 'Algo falló');
-		}
-	},
-	inscribir: async ({ request }) => {
-		const data = await request.formData();
-
-		let url = new URL(`http://localhost:8000/pokemons/${data.get('id')}/inscribir_alumno/`);
-		let params = { padron: data.get('padron') };
-		url.search = new URLSearchParams(params).toString();
-
-		const response = await fetch(url, {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' }
-		});
-		if (!response.ok) {
-			error(response.status, 'Algo falló');
-		}
-	}
-};*/
